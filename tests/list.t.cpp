8a7305bda73c3f5746b9d6e5d01fce376a9d9eb6
Shayne Fletcher <shayne.fletcher.50@gmail.com>
2015-11-15 00:18:01 -0500

    implement fold_left and rev

--- tests/list.t.cpp
+++ tests/list.t.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 
+#include <sstream>
+#include <iostream>
+
 #include "sum.hpp"
 
 namespace {
@@ -56,46 +59,96 @@ namespace {
   template <class T>
   inline T const& hd (list<T> const& l) {
     return l.match <T const&> (
-      [] (cons_t<T> const& n) -> T const& { return n.hd; }, 
+      //case : nil_t
       [] (nil_t const&) -> T const& { 
-        throw std::runtime_error{"hd"}; return 0;} 
+        throw std::runtime_error{"hd"}; } 
+      //case : cons_t<T>                         
+      [] (cons_t<T> const& n) -> T const& { 
+        return n.hd; }, 
     );
   }
 
   template <class T>
   inline list<T> const& tl (list<T> const& l) {
     return l.match <list<T> const&> (
-      [] (cons_t<T> const& n) -> list<T> const& { return n.tl; }, 
+      //case : nil_t
       [] (nil_t const&) -> list<T> const& { 
-        throw std::runtime_error{"tl"}; return *(list<T>*)0; } 
+        throw std::runtime_error{"tl"}; } 
+      //case : cons_t<T>                         
+      [] (cons_t<T> const& n) -> list<T> const& { 
+        return n.tl; }, 
     );
   }
-
 
 #if defined (_MSC_VER)
 #  pragma warning(pop)
 #endif//defined(_MSC_VER)
 
-  /*
-    let rec fold_left f acc xs =
-      match xs with
-      | [] -> acc
-      | (hd :: tl) -> fold_left f (f acc hd) tl
-
+  //fold_left
   template <class F, class AccT, class T>
-  decltype(auto) fold_left (F f, AccT acc, list<T> const& l) {
-
+  AccT fold_left (F f, AccT acc, list<T> const& l) {
+    return l.match<AccT>(
+      //case : nil_t
+      [=](nil_t) -> AccT { return acc; },
+      //case : cons_t<T>
+      [=](cons_t<T> const& x) -> AccT { 
+        return fold_left (f, f(acc, x.hd), x.tl); }
+      );
   }
+
+  //rev
+  template<class T>
+  list<T> rev (list<T> const& l) {
+    return fold_left(
+        [](auto const& acc, auto const& x) -> auto { 
+          return list<T>{constructor<cons_t<T>>{}, x, acc}; }
+      , nil<T>()
+      , l
+   );
+  }
+
+  /*
+  TODO:'range'
+
+  let range s e =
+    let rec loop acc s e =
+      if s >= e then acc
+      else loop (s :: acc) (s + 1) e 
+    in List.rev (loop [] s e)
    */
-  
+
+  template <class T>
+  std::string string_of_list (list<T> const& l) {
+    std::ostringstream os;
+    l.match<std::ostream&> (
+
+      //case cons:
+      [&](cons_t<T> const& e) -> auto& { 
+        return os << "cons ("<< e.hd << ", " << string_of_list (e.tl) << ")";  
+      },
+      //case nil:
+      [&](nil_t const& e) -> auto& { return os << "nil"; }
+
+    );
+
+    return os.str();
+  }
+
+  template <class T>
+  std::ostream& operator<< (std::ostream& os, list<T> const& l) {
+    return os << string_of_list (l) << std::endl;
+  }
 
 }//namespace
 
 TEST (pgs, list) {
 
-  list<int> l = cons (1, nil<int> ());
-  list<int> const& m = tl (l);
+  list<int> l = cons (2, cons (1, nil<int> ()));
+  list<int> const& m = tl (tl (l));
 
-  ASSERT_EQ (hd (l), 1);
+  //std::cout << reverse (l) << std::endl;
+
+  ASSERT_EQ (reverse (l), cons (1, cons (2, nil<int> ())));
+  ASSERT_EQ (hd (l), 2);
   ASSERT_EQ (m, nil<int>());
 }
