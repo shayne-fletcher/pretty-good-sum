8b27f225a245ce825aa140e76aee5d1fd39a4700
Shayne Fletcher <sfletcher13@bloomberg.net>
2015-11-16 13:28:25 -0500

    implement range (called rg due to conflict

--- tests/list.t.cpp
+++ tests/list.t.cpp
@@ -9,21 +9,31 @@ namespace {
   using namespace pgs;
   
   //type list = Cons of 'a * 'a list | Nil
-  template <class T> struct cons_t; // Case 2
+
+  template <class T> struct cons_t; // Case 1
   struct nil_t {};  // Case 2
 
-  //equaility of nil_t values
+  //Equaility of nil_t values
   bool operator==(nil_t const&, nil_t const&) {
     return true;
   }
   
-  //Since cons_t<T> values contain list_t<values>, cons_t<T> (and so
-  //list_t<T>) is a recursive type
+  //Since `cons_t<T>` values contain `list_t<values>`, `cons_t<T>`
+  //(and so `list_t<T>`) is a recursive type
   template <class T>
   using list = sum_type <recursive_wrapper<cons_t<T>>, nil_t>;
 
-  //list<T> is now defined and so we may go ahead and "fill in" the
-  //definition of cons_<T>
+  //It's helpful to have a trait that can "get at" the type `T`
+  //contained by a list type `L`
+  template <class L>
+  struct list_value_type;
+  template <class T>
+  struct list_value_type<list<T>> { typedef T type; };
+  template <class L>
+  using list_value_type_t = typename list_value_type<L>::type;
+
+  //A definition for `list<T>` now being available, we can "fill in"
+  //the definition of `cons_<T>`
   template <class T> 
   struct cons_t {
     T hd;
@@ -36,29 +46,29 @@ namespace {
 
   };
 
- //equality of cons_t<T> values
+ //Equality of `cons_t<T>` values
  template <class T>
   bool operator== (cons_t<T> const& l, cons_t<T> const& r) {
     return l.hd == r.hd && l.tl == r.tl;
   }
 
- //non-equality of cons_t<T> values
+ //Non-equality of `cons_t<T>` values
   template <class T>
   bool operator!= (cons_t<T> const& l, cons_t<T> const& r) {
     return !(l == r);
   }
 
-  //A factory function for a nil_t (a constant)
+  //A factory function for a `nil_t` (a constant)
   template <class T>
-  inline list<T> nil () {
+  constexpr list<T> nil () {
     return list<T>{constructor<nil_t>{}};
   }
 
-  //Factory function for a cons_t<T>
-  template <class T>
-  inline list<T> cons (T&& t, list<T>&& l) {
-    return list<T>{
-      constructor<cons_t<T>>{}, std::forward<T> (t), std::forward<list<T>>(l)};
+  //Factory function for a `cons_t<T>`
+  template <class U, class V>
+  inline std::remove_cv_t<std::remove_reference_t<V>> cons (U&& hd, V&& tl) {
+    using t = list_value_type_t<std::remove_cv_t<std::remove_reference_t<V>>>;
+    return list<t>{constructor<cons_t<t>>{}, std::forward<U>(hd), std::forward<V>(tl) };
   }
 
 #if defined (_MSC_VER)
@@ -98,7 +108,7 @@ namespace {
 
   //fold_left
   template <class F, class AccT, class T>
-  AccT fold_left (F f, AccT acc, list<T> const& l) {
+  AccT fold_left (F f, AccT const& acc, list<T> const& l) {
     return l.match<AccT>(
       //case : nil_t
       [=](nil_t){ return acc; },
@@ -119,15 +129,17 @@ namespace {
    );
   }
 
-  /*
-  TODO:'range'
+  namespace detail {
+    list<int> range_aux (list<int> const& acc, int const s, int e) {
+      if (s >= e)
+        return acc;
+      return range_aux (cons (s, acc), s + 1, e);
+    };
+  }//namespace detail
 
-  let range s e =
-    let rec loop acc s e =
-      if s >= e then acc
-      else loop (s :: acc) (s + 1) e 
-    in List.rev (loop [] s e)
-   */
+  list<int> rg (int begin, int end) {
+    return rev (detail::range_aux (nil<int> (), begin, end));
+  };
 
   //string_of_list
   template <class T>
@@ -157,10 +169,8 @@ namespace {
 
 TEST (pgs, list) {
 
-  list<int> l = cons (2, cons (1, nil<int> ()));
+  list<int> l = rg (1, 4);
   list<int> const& m = tl (tl (l));
 
-  ASSERT_EQ (rev (l), cons (1, cons (2, nil<int> ())));
-  ASSERT_EQ (hd (l), 2);
-  ASSERT_EQ (m, nil<int>());
+  ASSERT_EQ (rev (l), cons (3, cons (2, cons (1,  nil<int> ()))));
 }
