c45ef0d7b45da6eba1fe9b61c6e5fbd80ef2abad
Shayne Fletcher <sfletcher13@bloomberg.net>
2015-12-15 15:21:24 -0500

    checkpoint

--- tests/tree.t.cpp
+++ tests/tree.t.cpp
@@ -4,6 +4,9 @@
 
 #include <string>
 #include <utility>
+#include <numeric>
+#include <iterator>
+#include <iostream>
 
 namespace {
 
@@ -71,11 +74,25 @@ namespace {
        );
     }
 
+    //Apply 'f' to each binding in the tree
+    template <class AccT, class F>
+    AccT fold (AccT const& z, F f) const {
+      return impl_.match<AccT>(
+       [&z](empty_type const&) -> AccT { return z; },
+       [=, &z](node_type const& n) -> AccT {
+         auto const& b = n.data;
+         auto const& l = n.left_child;
+         auto const& r = n.right_child;
+         return self_type{r}.fold(f (self_type{l}.fold (z, f), b), f);
+       }
+     );
+    }
+
     //Compute a new tree with the same bindings as self but also
     //containing the binding `(k, v)`. If `k` was already bound in
     //self, that binding is replaced by `(k, v)`
     template <class P>
-    binary_search_tree<K, V> insert (P&& p) const {
+    self_type insert (P&& p) const {
       return impl_.match<self_type> (
         [&p](empty_type) { 
           tree_type n{
@@ -106,17 +123,147 @@ namespace {
         }
       );
     }
+
+    //Compute a new tree with same bindings as self but without a
+    //binding `k`
+    self_type remove (K const& k) const {
+      return fold (self_type {},
+        [&k](self_type const& acc, value_type const& p) {
+          return k == p.first ? acc : acc.insert (p);
+      });
+    }
+
+    //Compute the number of bindings in the tree
+    std::size_t size () const {
+      return fold (std::size_t{0}
+          , [](std::size_t acc, auto const&) { return ++acc; });
+    }
+
+    //True if `k` is bound
+    bool contains (K const& k) const {
+      return impl_.match<bool>(
+        [](empty_type) { return false; },
+        [&k](node_type const& n) {
+          auto const& a = n.data.first;
+          auto const& l = n.left_child;
+          auto const& r = n.right_child;
+          if (k == a) return true;
+          if (k < a) return self_type{l}.contains (k);
+          return self_type{r}.contains (k);
+        }
+      );
+    }
+
+    //Get the value bound to `k` in the tree. Raise a
+    //`std::runtime_error` if there is no binding for `k`
+    V const& lookup (K const& k) const {
+      return impl_.match<V const&>(
+        [](empty_type const&) -> V const& { throw std::runtime_error{"lookup"}; },
+        [&k](node_type const& n) -> V const& { 
+          auto const& a = n.data.first;
+          auto const& l = n.left_child;
+          auto const& r = n.right_child;
+          if (k == a) return n.data.second;
+          if (k < a) return self_type{l}.lookup (k);
+          return self_type{r}.lookup (k); }
+      );
+    }
+
+    //Extract the bindings in the tree. They'll come back sorted
+    //lexicographicaly on keys
+    template <class ItT>
+    ItT bindings (ItT dst) const {
+      return fold (dst, [](auto dst, auto const& n) { return *dst++ = n; });
+    }
+
+    //Check if all the bindings in the tree satisfy the predicate `p`
+    template <class P>
+    bool for_all (P const& p) const {
+      return fold (true, [&p](auto acc, auto const& n) { return acc && p (n); });
+    }
+
+    //True if at least one binding satisfies the predicate `p`
+    template <class P>
+    bool exists (P const& p) const {
+      return fold (false, [&p](auto acc, auto const& n) { return acc || p (n); });
+    }
   };
+
+  //Factory function for creating a tree from a list
+  template <class ItT>
+  auto mk_tree (ItT begin, ItT end) {
+    using node_t = typename std::iterator_traits<ItT>::value_type;
+    using key_t = typename node_t::first_type;
+    using val_t = typename node_t::second_type;
+    using tree_t = binary_search_tree<key_t, val_t>;
+
+    tree_t l;
+    for (; begin != end; l = l.insert(*begin++));
+
+    return l;
+  }
 
 }//namespace<anonymous>
 
-TEST (pgs, tree) {
+TEST (pgs, tree_basic) {
 
-  binary_search_tree<int, std::string> t;
+  using tree_t = binary_search_tree<int, std::string>;
+
+  tree_t t;
 
   ASSERT_TRUE(t.empty ());
 
-  binary_search_tree<int, std::string> tt = 
-    t.insert (std::make_pair (1, std::string ("foo")));
+  tree_t tt = t.insert (
+   std::make_pair (1, std::string ("foo"))).
+    insert (std::make_pair (2, std::string ("bar")));
 
+  ASSERT_EQ(tt.size (), 2);
+
+  tree_t ttt = tt.remove (2);
+
+  ASSERT_EQ(ttt.size (), 1);
+}
+
+namespace std {//Hack!
+
+  template <class K, class V>
+  std::ostream& operator << (std::ostream& os, std::pair<K, V> const& p) {
+    return os << "(" << p.first << ", " << p.second << ")";
+  }
+
+}//namspace std
+
+
+TEST (pgs, tree_more) {
+
+  using node_t = std::pair<std::string, int>;
+  using tree_t = binary_search_tree<std::string, int>;
+
+  node_t data[] = {
+      std::make_pair (std::string {"mike"}, 45),
+      std::make_pair (std::string {"sally"}, 25),
+      std::make_pair (std::string {"sebastien"}, 33),
+      std::make_pair (std::string {"jack"}, 12),
+      std::make_pair (std::string {"henry"}, 66)
+  };
+
+  tree_t ages = mk_tree (data, data + 5);
+
+  EXPECT_EQ (ages.size (), 5);
+  EXPECT_TRUE (ages.contains (std::string {"sebastien"}));
+  EXPECT_EQ (ages.lookup (std::string {"sally"}), 25);
+  EXPECT_THROW (ages.lookup (std::string {"gru"}), std::runtime_error);
+
+  ages.bindings (
+    std::ostream_iterator<std::pair<std::string, int>>(std::cout, " ")
+  );
+
+  auto everyone_of_age = [](auto const& b) { return b.second > 21; };
+  EXPECT_FALSE (ages.for_all (everyone_of_age));
+  auto no_senior_citizen = [](auto const& b) { return b.second < 70; };
+  EXPECT_TRUE (ages.for_all (no_senior_citizen));
+  auto sebastien = [](auto const& b) { return b.first == "sebastien"; };
+  EXPECT_TRUE (ages.exists (sebastien));
+  auto gru = [](auto const& b) { return b.first == "gru"; };
+  EXPECT_FALSE (ages.exists (gru));
 }
